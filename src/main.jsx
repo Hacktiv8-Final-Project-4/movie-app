69342f27df2f16ab3f16f94ed33911cebb8028fb
ekialfani <ekialfani15@gmail.com>
2023-06-10 23:50:23 +0800

    add: search page and react-router configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,24 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Search from "./pages/Search";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "search",
+    element: <Search />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
