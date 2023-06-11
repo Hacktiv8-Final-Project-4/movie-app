f5b6836ecc475c60c44b19d3053077150f0d4e7d
ekialfani <ekialfani15@gmail.com>
2023-06-11 22:16:13 +0800

    feat: implement movie search based on user input.
    This commit introduces the feature to perform movie searches based on user input. Users can now search for movies by entering their desired keywords or criteria.

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,14 +2,19 @@ import { useEffect } from "react";
 import { searchMoviesByTitle } from "../features/searchSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Header from "../components/Header";
+import { useParams } from "react-router-dom";
+import slicedParams from "../utils/slicedParams";
 
 export default function Search() {
   const dispatch = useDispatch();
-
+  const { searchParams } = useParams();
+  const title = slicedParams(searchParams, 7, searchParams.length);
+  
   const movies = useSelector((state) => state.search);
+
   useEffect(() => {
-    dispatch(searchMoviesByTitle("avengers"));
-  }, [dispatch]);
+    dispatch(searchMoviesByTitle(title));
+  }, [title, dispatch]);
 
   console.log(movies?.searchResults);
 
