2d690708f64b1eb531f19ea07e835dc90e9b3bd8
ekialfani <ekialfani15@gmail.com>
2023-06-12 21:59:31 +0800

    feat(storage): adding redux persist configuration to store favorite movies in local storage

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { testSlice } from "../features/testSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import { combineReducers } from "redux";
+import { persistConfig } from "../config/persistConfig";
 import searchReducer from "../features/searchSlice";
 import favoritesReducer from "../features/favoritesSlice";
 
-export const store = configureStore({
-  reducer: {
-    test: testSlice.reducer,
-    search: searchReducer,
-    favorites: favoritesReducer,
-  },
+const rootReducer = combineReducers({
+  search: searchReducer,
+  favorites: favoritesReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./app/App.jsx";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./app/App.jsx";
 import Search from "./pages/Search";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Favorites from "./pages/Favorites";
@@ -27,7 +28,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
