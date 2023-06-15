2046a5e75ce5bec9b8618347015d1cefacedd850
ekialfani <ekialfani15@gmail.com>
2023-06-16 00:30:44 +0800

    feat: adding a background color to the header when scrolling the page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [title, setTitle] = useState("");
+  const [scrolled, setScrolled] = useState(false);
 
   const handleInputChange = (event) => {
     setTitle(event.target.value);
@@ -20,13 +21,26 @@ const Header = () => {
       }
     }, 500);
 
-    return () => clearTimeout(timeOut);
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      clearTimeout(timeOut)
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [dispatch, navigate, title]);
 
   return (
     <header>
-      <nav className="border-gray-200 dark:bg-gray-900 fixed z-50 top-0 left-0 right-0">
-        <div className="max-w-screen-xl xl:max-w-6xl flex items-center mx-auto p-4 xl:px-0 gap-x-10">
+      <nav className={`${scrolled ? "bg-gray-900 shadow-lg" : "bg-transparent"} border-gray-200 dark:bg-gray-900 fixed z-50 top-0 left-0 right-0`}>
+        <div className="max-w-screen-xl xl:max-w-6xl flex items-center mx-auto p-5 xl:px-0 gap-x-10">
           <NavLink to="/" 
             className="w-max text-2xl font-semibold text-white flex items-center gap-x-3">
             <i className="fa-solid fa-circle-play text-yellow-500"></i>
@@ -50,7 +64,7 @@ const Header = () => {
             <li className="w-full flex justify-end">
               <div className="flex md:order-2">
                 <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" >
-                  <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                <i className="fa-solid fa-magnifying-glass text-slate-500"></i>
                   <span className="sr-only">Search</span>
                 </button>
                 <div className="relative hidden md:block">
@@ -61,7 +75,7 @@ const Header = () => {
                   <input 
                     type="text" 
                     id="search-navbar" 
-                    className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-400 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."
+                    className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-yellow-400 focus:border-yellow-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."
                     autoComplete="off"
                     onChange={handleInputChange}
                   />
