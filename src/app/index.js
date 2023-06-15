252ea3e4614103341be28fd0a8438e2ade579ba9
claradivap512 <ClaraParamitha512@gmail.com>
2023-06-15 16:45:25 +0700

    carousel and header

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { testSlice } from "../features/testSlice";
-
-export const store = configureStore({
-  reducer: {
-    test: testSlice.reducer,
-  },
-});
