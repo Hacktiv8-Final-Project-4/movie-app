9b2e65e7b39ed68c730a9dfd76676edbaaa2800b
ekialfani <ekialfani15@gmail.com>
2023-06-16 15:33:02 +0800

    refactor: change the base url protocol to https.

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
 const CONFIG = {
-  BASE_URL: "http://www.omdbapi.com/",
+  BASE_URL: "https://www.omdbapi.com/",
   API_KEY: import.meta.env.VITE_API_KEY,
 }
 
 export default CONFIG;
-
